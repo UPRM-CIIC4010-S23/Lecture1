9d4b04a26016a48ec65e09bd5f3409aa75d3056b
Bienvenido Velez <Bienvenido.Velez@upr.edu>
2023-02-06 18:53:17 -0400

    added sumAreas and getLargest

--- triangleArea.cpp
+++ triangleArea.cpp
@@ -19,6 +19,32 @@ bool largerThan(double t1s1, double t1s2, double t1s3,
 }
 
 
+double sumAreas(double s1[], double s2[], double s3[], int size) {
+    double areaTotal = 0;
+    for (int i=0; i<size; i++) {
+        areaTotal += getArea(s1[i],s2[i],s3[i]);
+    }
+    return areaTotal;
+}
+
+double *getLargest(double s1[], double s2[], double s3[], int size) {
+
+    if (size <= 0) return nullptr;
+    double largestArea = getArea(s1[0],s2[0],s3[0]);
+    int largestPos = 0;
+    for (int i = 1; i<size; i++) {
+        double nextArea = getArea(s1[i],s2[i],s3[i]);
+        if (nextArea > largestArea) {
+            largestArea = nextArea;
+            largestPos = i;
+        }
+    }
+
+    double *largestTriangle = new double[3] { s1[largestPos], s2[largestPos], s3[largestPos] };
+    return largestTriangle;
+}
+
+
 
 double getAngle1(double s1, double s2, double s3) {
 
